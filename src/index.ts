cda7b40d2daa97d41876c664f087501603110fdb
Veeti Poutsalo <veeti.poutsalo@aalto.fi>
2021-01-20 13:08:13 +0000

    Implement HTTPS support via devcert

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import * as devcert from 'devcert'
+import https from 'https'
 import app from './app'
 
 const port = process.env.PORT || 3000
+const useHTTPS = process.env.HTTPS === 'true'
 
-app.listen(port, () => {
+const listenCallback = () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${port}`)
-})
+}
+
+const runServer = async () => {
+  if (useHTTPS) {
+    const ssl = await devcert.certificateFor('pakettikauppa-backend')
+    https.createServer(ssl, app).listen(port, listenCallback)
+  } else {
+    app.listen(port, listenCallback)
+  }
+}
+
+runServer()
